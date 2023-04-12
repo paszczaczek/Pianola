1b0431f015043688800a664cee6503b1b097ba99
Marcin Paśnikowski <marcin.pasnikowski@mccomp.pl>
2023-04-12 10:42:37 +0200

    Glyph.IsHelperLinesVisibleProperty

--- Pianola/Glyph.cs
+++ Pianola/Glyph.cs
@@ -2,6 +2,7 @@
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
+using System.Windows.Shapes;
 
 namespace Pianola;
 
@@ -21,6 +22,30 @@ public class Glyph : TextBlock
     public const string BlackNoteHead = "\x0056";
     public const string WhiteNoteHead = "\x0055";
 
+    private static readonly SolidColorBrush GuidLinesBrush = new(Brushes.Violet.Color) { Opacity = 0.3 };
+
+    #region IsHelperLinesVisibleProperty
+
+    public static readonly DependencyProperty IsGuidLinesVisibleProperty = DependencyProperty.Register(
+        nameof(IsGuidLinesVisible), typeof(bool), typeof(Glyph),
+        new FrameworkPropertyMetadata(false,
+            (d, e) =>
+            {
+                var glyph = (Glyph)d;
+                var isHelperLinesVisible = (bool)e.NewValue;
+                glyph.Background = isHelperLinesVisible
+                    ? GuidLinesBrush
+                    : Brushes.Transparent;
+            }));
+
+    public bool IsGuidLinesVisible
+    {
+        get => (bool)GetValue(IsGuidLinesVisibleProperty);
+        set => SetValue(IsGuidLinesVisibleProperty, value);
+    }
+
+    #endregion
+
     static Glyph()
     {
         // wyznacz baseline i wysokość główki nuty (przy starcie aplikacji)
@@ -39,6 +64,13 @@ public class Glyph : TextBlock
     {
         FontFamily = new FontFamily(FamilyName);
         base.FontSize = FontSize;
-        // Background = Brushes.Aquamarine;
+
+        var line = new Line
+        {
+            X1 = 0, Y1 = 10, X2 = 10, Y2 = 10 ,
+            Stroke = Brushes.Black, 
+            StrokeThickness = 10
+        };
+        Inlines.Add(line);
     }
 }
