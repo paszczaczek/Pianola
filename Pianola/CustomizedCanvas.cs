c6d8918da8ed07f4373c306769502a994dc2ec1c
Marcin Paśnikowski <marcin.pasnikowski@mccomp.pl>
2023-04-13 12:21:08 +0200

    dodany CustomizedCanvas.cs z bindingiem

---
+++ Pianola/CustomizedCanvas.cs
@@ -0,0 +1,63 @@
+﻿using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Pianola;
+
+// TODO czy tego kodu nie da się zamienić na xaml?
+public class CustomizedCanvas : Canvas
+{
+    #region IsGridLinesVisibleProperty
+
+    public static readonly DependencyProperty IsGridLinesVisibleProperty = DependencyProperty.Register(
+        nameof(IsGridLinesVisible), typeof(bool), typeof(CustomizedCanvas),
+        new FrameworkPropertyMetadata(
+            default(bool),
+            IsGridLinesVisiblePropertyChangedCallback));
+
+    private static void IsGridLinesVisiblePropertyChangedCallback(
+        DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        // zmieniła się prarametr określający czy wyświetlać liniie pomocnicze
+        var customizedCanvas = (CustomizedCanvas) d;
+        var newIsVisible = (bool) e.NewValue;
+        if (newIsVisible)
+        {
+            // linie mają być wyświetlane
+
+            // dodaj ramkę wokół
+            var border = new Border
+            {
+                // Width = customizedCanvas.Width,
+                // Height = customizedCanvas.Height,
+                BorderBrush = Brushes.BlueViolet,
+                BorderThickness = new Thickness(1),
+                SnapsToDevicePixels = true
+            };
+            customizedCanvas.Children.Add(border);
+            
+            SetLeft(border, 0);
+            SetTop(border, 0);
+            
+            // zbinduj wymiary ramki z wymiarami płótna
+            border.SetBinding(WidthProperty, new Binding(nameof(Width)) {Source = customizedCanvas});
+            border.SetBinding(HeightProperty, new Binding(nameof(Height)) {Source = customizedCanvas});
+        }
+        else
+        {
+            // linie mają nie być wyświetlane - usuń je
+            var border = customizedCanvas.Children.OfType<Border>().First();
+            customizedCanvas.Children.Remove(border);
+        }
+    }
+
+    public bool IsGridLinesVisible
+    {
+        get => (bool) GetValue(IsGridLinesVisibleProperty);
+        set => SetValue(IsGridLinesVisibleProperty, value);
+    }
+
+    #endregion
+}
--- Pianola/Scale.cs
+++ Pianola/Scale.cs
@@ -1,82 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 
 namespace Pianola;
-
-public class Scale : Canvas
-{
-    public enum Type
-    {
-        Ces,
-        Cis
-    }
-
-    #region IsHelperLinesVisibleProperty
-
-    public static readonly DependencyProperty IsGuidLinesVisibleProperty = DependencyProperty.Register(
-        nameof(IsGuidLinesVisible), typeof(bool), typeof(Scale),
-        new FrameworkPropertyMetadata(
-            default(bool),
-            (d, e) =>
-            {
-                // zmieniła się prarametr określający czy wyświetlać linie pomocnicze
-                var scale = (Scale) d;
-                var isVisible = (bool) e.NewValue;
-                //scale.Chromatics.IsGuidLinesVisible = isVisible;
-            }));
-
-
-    public bool IsGuidLinesVisible
-    {
-        get => (bool) GetValue(IsGuidLinesVisibleProperty);
-        set => SetValue(IsGuidLinesVisibleProperty, value);
-    }
-
-    #endregion
-
-    public static Scale Create(Type type)
-    {
-        return type switch
-        {
-            Type.Ces => new CesScale(),
-            Type.Cis => new CisScale(),
-            _ => throw new ArgumentOutOfRangeException(nameof(type), type, null)
-        };
-    }
-
-    protected Scale(Chromatic.Type chromaticType, IList<double> chromaticTops)
-    {
-        for (var i = 0; i < chromaticTops.Count; i++)
-        {
-            // dodaj do canvas znak chromatyczny
-            var chromatic = Chromatic.Create(chromaticType, chromaticTops[i]);
-            Children.Add(chromatic);
-
-            SetLeft(chromatic, i * 20); // OnRender ustawi prawdziwe wartości
-            Width = 100;
-        }
-    }
-    
-    private IEnumerable<Chromatic> Chromatics => Children.OfType<Chromatic>();
-
-
-    protected override void OnRender(DrawingContext dc)
-    {
-        // rozmieszczamy znaki chromatyczne w poziome, tak by były jeden za drugim
-        var width = .0;
-        foreach (FrameworkElement chromatic in Children)
-        {
-            SetLeft(chromatic, width);
-            width += chromatic.ActualWidth;
-        }
-
-        Width = width;
-    }
-}
 
 public class CesScale : Scale
 {
@@ -107,5 +35,44 @@ public class CisScale : Scale
         Staff.TopOf(Staff.Line.Second)
     })
     {
+    }
+}
+
+public class Scale : CustomizedCanvas
+{
+    public enum Type
+    {
+        Ces,
+        Cis
+    }
+
+    protected Scale(Chromatic.Type chromaticType, IList<double> chromaticTops)
+    {
+        for (var i = 0; i < chromaticTops.Count; i++)
+        {
+            // dodaj do canvas znak chromatyczny
+            var chromatic = Chromatic.Create(chromaticType, chromaticTops[i]);
+            Children.Add(chromatic);
+
+            SetLeft(chromatic, i * 20); // OnRender ustawi prawdziwe wartości
+            Width = 100;
+        }
+    }
+    
+    private IEnumerable<Chromatic> Chromatics => Children.OfType<Chromatic>();
+
+    // TODO zamienić OnRener na OnVisualChildrenChanged
+    protected override void OnRender(DrawingContext dc)
+    {
+        // rozmieszczamy znaki chromatyczne w poziome, tak by były jeden za drugim
+        var width = .0;
+        foreach (FrameworkElement chromatic in Children)
+        {
+            if (chromatic is Border) continue; // TODO
+            SetLeft(chromatic, width);
+            width += chromatic.ActualWidth;
+        }
+
+        Width = width;
     }
 }
