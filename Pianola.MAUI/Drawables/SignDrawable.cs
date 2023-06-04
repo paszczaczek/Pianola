219604d7efd168523eaa88696dfc95c4ef8e0ee2
Marcin Paśnikowski <marcin.pasnikowski@mccomp.pl>
2023-06-04 14:23:51 +0200

    przeniesienie SignInfo do SignDrawable

--- Pianola.MAUI/Drawables/SignDrawable.cs
+++ Pianola.MAUI/Drawables/SignDrawable.cs
@@ -70,4 +70,22 @@ public static class SignDrawable
             textFlow: TextFlow.OverflowBounds,
             lineSpacingAdjustment: 0);
     }
+
+    private class SignInfo
+    {
+        public readonly string Code;
+        public readonly double AscenderRatio;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="code"></param>
+        /// <param name="ascender">Odczytany z FontForge, wyrażony w jego wewnętrznych współrzędnych.</param>
+        /// <param name="descender">Odczytany z FontForge, wyrażony w jego wewnętrznych współrzędnych.</param>
+        public SignInfo(string code, double ascender, double descender)
+        {
+            Code = code;
+            AscenderRatio = ascender / (ascender + descender);
+        }
+    }
 }
